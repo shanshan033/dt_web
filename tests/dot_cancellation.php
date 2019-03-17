287cb131b8b3c42a9bf4b23da8b53a55818647bc
shanshan <lishanshan.unnc@aiesec.net>
2019-03-17 15:19:45 +0800

    simplify dot cancellation code

--- tests/dot_cancellation.php
+++ tests/dot_cancellation.php
@@ -1,13 +1,12 @@
 <?php 
 session_start();
-if(empty($_SESSION["name"]))               //判断session里面是不是存储到值，如果没有存储，让其跳转到登录界面
+if(empty($_SESSION["name"]))               
 {
     header("location:../login/login.html");
     exit();
 }else{
   $name = $_SESSION["name"];  
 }
-
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -18,148 +17,17 @@ if(empty($_SESSION["name"]))               //判断session里面是不是存储�
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Dot Cancellation Task</title>
     <link href="css/demo.css" rel="stylesheet" type="text/css">
-	<link href="css/attention_task.css" rel="stylesheet" type="text/css">
-    <!-- <script src="js/jquery-1.10.2.min.js" type="text/javascript"></script> -->
+	  <link href="css/attention_task.css" rel="stylesheet" type="text/css">
     <!--Framework-->
     <script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
     <script src="js/jquery-ui.js" type="text/javascript"></script>
     <!--End Framework-->
-     <script src="js/jquery.ffform.js" type="text/javascript"></script>
-     <!-- document.getElementById("clock").value = timer; -->
-
-<script type="text/javascript">
-
-  //Initialize variables
-  // var userName = $('#nameFooter').html();
-  var correctNumber = 0;
-  var totalCorrectNumber = 0;
-  var errorNumber = 0;
-  var totalTimer = 0;
-
-   // Record the time
-   function clock() {         
-      totalTimer +=1;
-      // document.getElementById("clock").value = timer;
-   }
-   setInterval(clock,10);
-
-   
-
-
-	$(document).ready(function(){
-
-    // Create a 10x10 table containing images
-    var generate = function (){
-    var tableData = "";
-    for (var j=0; j<10; j++){
-        tableData+="<tr>";
-        for(var i=0;i<10;i++){
-          // Images are randomly distributed into the table
-          var id=Math.round(Math.random()*6)+1;
-          var data = "<input type='image' src=images/img"+id+".png class = 'imgClass' id='img"+id+"' >";
-          tableData+="<td>"+data+"</td>";
-          if (id == 0 || id == 3 || id == 4){
-            totalCorrectNumber +=1;
-          }
-
-        }
-        tableData+="</tr>";
-      }
-
-      return tableData;
-    }
-
-    // Show table into web page
-  	$("#tbody1").html(generate());
-
-      /* Check whether the choice is right (this alert will not be shown in ths final version)
-  		 * Obtain the id of the image and check the correction
-       */
-
-  	$("table tr td").on("click", function () {
-          var tdidx = $(this).index();
-          var td = $(this).find("td:eq("+tdidx+")").context.childNodes[0].id;
-          $(this).find("input").addClass("darken");
-          $(this).find('input').attr('disabled', true);
-          console.log($(this).find("td:eq("+tdidx+")").context.childNodes[0].id);
-          
-          if (td == "img0" || td == "img3" || td == "img4"){
-          	// alert("You are right!");
-            correctNumber +=1;
-          }
-          else{            	
-            // alert("You are wrong!");
-            errorNumber +=1;
-          }
-
-        if(correctNumber == totalCorrectNumber || errorNumber >= (100-totalCorrectNumber)){
-            // pass database
-            // alert("error: "+errorNumber+" total correct: "+totalCorrectNumber);
-
-            $.ajax({
-            type: 'POST',
-            url: "dot_cancellation_store.php",
-            dataType: "json",
-            data: {username:function() {
-                return $('#nameFooter').html()},
-                correct_number: correctNumber,
-                error_number: errorNumber,
-                total_correct_number: totalCorrectNumber,
-                answer_time: totalTimer},
-            success: function(){
-                setTimeout("window.location.href='schulte_table/schulte_table.html'", 500);
-             
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                
-                    状态码
-                    console.log(XMLHttpRequest.status);
-                    // 状态
-                    console.log(XMLHttpRequest.readyState);
-                    // 错误信息   
-                    console.log(textStatus);
-
-                }
-        });
-
-          }
-      });
-  });
-</script>
-
+    <script src="js/jquery.ffform.js" type="text/javascript"></script>
+    <script src="js/dot_cancellation.js" type="text/javascript"></script>
 </head>
-<!-- 
-  <div class="timer">
-        <h3> Timer </h3>
-         <form>
-            <input type="text" id="clock" size="20" placeholder="0" />
-        </form> 
-    </div>
 
-    <div class="task-explanation">
-        <h3> Task Description </h3>
-            <p> Cross out all of the groups containing four dots with computer mouse</p>   
-    </div>
- -->
-<!--  <div class="clock">
-        <h3> Timer </h3>
-         <form>
-            <input type="text" id="clock" size="20" placeholder="0" />
-        </form> 
-    </div> -->
 <body style="">
-	<!-- <div class="demos-buttons">
-        <h3>
-            Attention Task </h3>
-        <a href="stroop_color.html" class="submit">Stroop Color</a><br />
-        <a href="#" class="submit">Mouse Tracking Task</a> 
-        <br>
-        <a href="#" class="submit active">Dot Cancellation Task </a> 
-        <br>
-        <a href="#" class="submit">Trail Marking Task </a><br>
-  </div> -->
-
-<div class="container bounceIn animated center-in-center-dot-cancellation">
+<div class="container bounceIn animated center-in-center-dot-cancellation" style="width : 75%">
 	<div class="row clearfix">
 		<div class="col-md-12 column">
 			<table class="table table-hover table-bordered">
--- tests/dot_cancellation_store.php
+++ tests/dot_cancellation_store.php
@@ -1,32 +1,18 @@
-<?
-    header("Content-Type: text/html; charset=utf8");
+<?php
+    include("../login/validation/dbConnection.php");
 
-   // include('connect.php');//connect database
-
-    $server="127.0.0.1";//主机
-    $db_username="root";//你的数据库用户名
-    $db_password="Lxy551812";//你的数据库密码
-    $db_name = "dt_web";
-    $con = mysqli_connect($server,$db_username,$db_password, $db_name);//链接数据库
-
-
-    if(!$con){
-        die("can't connect".mysqli_connect_error().PHP_EOL);//如果链接失败输出错误
-    }else{
-        // echo "connect success\n";
-    }
-  
     if(!isset($_POST['username'])){
-        // echo "false";
         echo json_encode(false);
-        // exit("Wrong execute");
     }else{
 
     // Obtain the value from form
+    $click_id = $_POST['click_id'];
+    $answer_flag = $_POST['answer_flag'];
     $correct_number = $_POST['correct_number'];
     $error_number = $_POST['error_number'];
     $total_correct_number = $_POST['total_correct_number'];
     $answer_time = $_POST['answer_time'];
+    $total_answer_time = $_POST['total_answer_time'];
     $name = $_POST['username'];
 
     $q = "SELECT * FROM users WHERE user_name = '$name' ";
@@ -38,21 +24,19 @@
     $test_times = $row[5];
 
     //Insert data into database
-    $q="INSERT INTO dot_cancellation (user_id, user_name, user_gender, user_age, test_group, correct_number, error_number, total_correct_number, answer_time) VALUES ($id,'$name', $gender, $age, $test_times, $correct_number, $error_number, $total_correct_number, '$answer_time')";
+    $q="INSERT INTO dot_cancellation (user_id, user_name, user_gender, user_age, test_group, click_id, answer_flag, answer_time, correct_number, error_number, total_correct_number, total_answer_time) VALUES ($id,'$name', $gender, $age, $test_times, $click_id, $answer_flag, $answer_time, $correct_number, $error_number, $total_correct_number, '$total_answer_time')";
 
     $reslut=mysqli_query($con, $q);
     
-    // echo "query success\n";
-
     if (!$reslut){
-        die('Error: ' . mysqli_error($con));//Error warming
+        die('Error: ' . mysqli_error($con)); // Error warming
     }else{
-
-
-        // echo "Success";//Success
-        mysqli_close($con);//Close database
-        echo json_encode(true);
-
+        mysqli_close($con); // Close database
+        if($correct_number == $total_correct_number){
+            echo json_encode(false);
+        }else{
+            echo json_encode(true);
+        }   
     }
 }
 
